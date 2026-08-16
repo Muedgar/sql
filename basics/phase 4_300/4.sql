3c4f71aa817361d5afec964ada36b9381f40c025
MuEdgar <edgarmutangana@gmail.com>
2026-08-16 23:52:35 +0200

    Learning advanced sql

--- basics/phase 4_300/4.sql
+++ basics/phase 4_300/4.sql
@@ -50,3 +50,59 @@ BEGIN
     VALUES (gen_random_uuid(), v_user_id, v_amount, 'PENDING', NOW());
 END $$;
 
+DO $$
+DECLARE
+ v_user_id UUID := 'USER_UUID_HERE';
+ v_amount NUMERIC := -500;
+BEGIN
+    IF v_amount <=0 THEN
+        RAISE EXCEPTION 'Order amount must be greater than 0';
+    END IF;
+
+    INSERT INTO orders (id, user_id, total_amount, status, created_at)
+    VALUES (gen_random_uuid(), v_user_id, v_amount, 'PENDING', NOW());
+END $$;
+
+DO $$
+DECLARE
+    v_user_id UUID := 'USER_UUID_HERE';
+    v_current_status TEXT;
+BEGIN
+    SELECT status
+    INTO v_current_status
+    FROM users
+    WHERE id = v_user_id;
+
+    IF v_current_status = 'ACTIVE' THEN
+        UPDATE users
+        SET status = 'ACTIVE'
+        WHERE id = v_user_id;
+    END IF;
+END $$;
+
+DO $$
+DECLARE
+    user_record RECORD;
+BEGIN
+    FOR user_record IN
+        SELECT id, email
+        FROM users
+        WHERE status = 'INACTIVE'
+    LOOP
+        RAISE NOTICE 'Inactive user: % - %', user_record.id, user_record.email;
+    END LOOP;
+END $$;
+
+DO $$
+DECLARE
+    v_updated_count INT;
+BEGIN
+    UPDATE orders
+    SET status = 'ARCHIVED'
+    WHERE status = 'PENDING'
+        AND created_at < NOW() - INTERVAL '90 days';
+
+    GET DIAGNOSTICS v_updated_count = ROW_COUNT;
+
+    RAISE NOTICE '% orders archived', v_updated_count;
+END $$;
